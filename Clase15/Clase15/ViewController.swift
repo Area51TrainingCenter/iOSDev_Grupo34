372f7b0c48a45551ce2c58a608a3ea0822e5b5f1
EduardoHoyos <edu.hoyos92@gmail.com>
2020-01-14 22:02:11 -0500

    Notification configuration

---
+++ Clase15/Clase15/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Clase15
+//
+//  Created by Alumno on 1/14/20.
+//  Copyright © 2020 Area51 Training Center. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Clase15/Pods/Target Support Files/Pods-Clase15/Pods-Clase15-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Clase15 : NSObject
+@end
+@implementation PodsDummy_Pods_Clase15
+@end
